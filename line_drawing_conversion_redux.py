42c8f8420551e8715033806b98c1b9658272c549
George Alvarez <grez72@gmail.com>
2023-12-06 15:28:34 -0500

    updated line drawing conversion

--- line_drawing_conversion_redux.py
+++ line_drawing_conversion_redux.py
@@ -3,132 +3,113 @@
 
 GAA's update of cluster_linedrawing_converstions.py
 
+https://github.com/carolineec/informative-drawings.git
+
 Original file is located at
     https://colab.research.google.com/github/robsymowen/Project-Line/blob/main/Collab/Cluster_LineDrawing_Conversions.ipynb
     
     Example Usage:
-    python cluster_linedrawing_converstions.py $SHARED_DATA_DIR/imagenet1k-256/val/n01440764 /n/alvarez_lab_tier1/Users/alvarez/datasets/imagenet1k-line/val/n01440764
+    python line_drawing_conversion_redux.py anime_style $SHARED_DATA_DIR/imagenet1k-256/val/n01440764 /n/alvarez_lab_tier1/Users/alvarez/datasets/imagenet1k-line/anime_style/val/n01440764
     
-    python cluster_linedrawing_converstions.py $SHARED_DATA_DIR/imagenet1k-256/val /n/alvarez_lab_tier1/Users/alvarez/datasets/imagenet1k-line/val
+    python line_drawing_conversion_redux.py anime_style $SHARED_DATA_DIR/imagenet1k-256/val /n/alvarez_lab_tier1/Users/alvarez/datasets/imagenet1k-line/anime_style/val
+    
+    styles = ['anime_style', 'contour_style', 'opensketch_style']
     
 """
-
-import argparse
-from glob import glob
 import os
+import argparse
+from urllib.parse import urlparse
+from glob import glob
+from pdb import set_trace
 import subprocess
 import shutil
+from fastprogress import progress_bar
 
-# Create the parser
-parser = argparse.ArgumentParser(description='Process line drawing conversion arguments.')
+styles = ['anime_style', 'contour_style', 'opensketch_style']
 
-# Add arguments
-parser.add_argument('root_dir', type=str, help='The root directory of the ImageNet dataset')
-parser.add_argument('output_dir', type=str, help='The directory where output will be stored')
+def parse_arguments():
+    """ Parse command line arguments for line drawing conversion. """
+    parser = argparse.ArgumentParser(description='Process line drawing conversion arguments.')
+    parser.add_argument('style', type=str, choices=styles, default='anime_style', 
+                        help='The style for line drawing conversion')
+    parser.add_argument('image_dir', type=str, help='The root directory of the ImageNet dataset')
+    parser.add_argument('output_dir', type=str, help='The directory where output will be stored')
 
-# Parse the arguments
-args = parser.parse_args()
+    return parser.parse_args()
 
-# Use the parsed arguments
-IMAGENET_ROOT_DIR = args.imagenet_root_dir
-OUTPUT_DIR = args.output_dir
-
-"""# **Set up Line Drawing Library**
-
-The biggest development for me here was parsing through the test.py file to edit the data directory being used and the number of files we would iterate through.
-- The edited area of the test.py file is labeled
-- The files are then copied over to the google drive.
-"""
-
-# Git clone command
-git_clone_command = "git clone https://github.com/carolineec/informative-drawings.git"
-
-# Run the command
-subprocess.run(git_clone_command, shell=True, check=True)
-
-# URL to model download.
-checkpoint_url = 'https://s3.us-east-1.wasabisys.com/visionlab-projects/transfer/informative-drawings/model.zip'
+def get_filename_from_url(url):
+    """ Extract the filename from a given URL. """
+    parsed_url = urlparse(url)
+    return os.path.basename(parsed_url.path)
 
 def download_and_unzip_model(checkpoint_url, destination_folder):
+    """ Download and unzip the line drawing model if not already present. """
+    # Check if the model or a key file already exists to avoid re-downloading
+    model_file_check = get_filename_from_url(checkpoint_url)
+    model_zip_path = os.path.join(destination_folder, model_file_check)
+    if os.path.exists(model_zip_path):
+        print(f"Model file {model_file_check} already exists, skipping download.")
+        return
+
     # Create the destination folder if it doesn't exist
     os.makedirs(destination_folder, exist_ok=True)
 
     try:
         # Download the model checkpoint zip file
-        subprocess.run(['wget', '-c', checkpoint_url, '-O', os.path.join(destination_folder, 'model.zip')], check=True)
+        subprocess.run(['wget', '-c', checkpoint_url, '-O', model_zip_path], check=True)
 
         # Unzip the model checkpoint
-        subprocess.run(['unzip', os.path.join(destination_folder, 'model.zip'), '-d', destination_folder], check=True)
+        subprocess.run(['unzip', model_zip_path, '-d', destination_folder], check=True)
 
-        # Move the contents of the 'model' folder to the destination
+        # Move contents from 'model' folder, if exists, to destination folder
         model_folder = os.path.join(destination_folder, 'model')
-        for item in os.listdir(model_folder):
-            s = os.path.join(model_folder, item)
-            d = os.path.join(destination_folder, item)
-            if os.path.isdir(s):
-                shutil.move(s, d)
-
-        # Remove the now-empty 'model' folder
-        os.rmdir(model_folder)
+        if os.path.exists(model_folder):
+            for item in os.listdir(model_folder):
+                shutil.move(os.path.join(model_folder, item), destination_folder)
+            os.rmdir(model_folder)
 
     except subprocess.CalledProcessError as e:
-        # Handle any errors (non-zero exit code)
-        print(f"Error executing command: {e}")
-        print(f"Command output (stderr): {e.stderr}")
+        print(f"Error downloading or unzipping model: {e}")
+        exit(1)
+        
+def get_folders_or_use_base_dir(data_dir):
+    """ Get a list of subdirectories in data_dir. If there are none, return a list with only data_dir. """
+    folders = [f for f in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, f))]
 
-# Example: Download and unzip the model
-checkpoint_url = 'https://s3.us-east-1.wasabisys.com/visionlab-projects/transfer/informative-drawings/model.zip'
-destination_folder = '/content/informative-drawings/checkpoints'
-download_and_unzip_model(checkpoint_url, destination_folder)
+    # If there are no subdirectories, use data_dir itself
+    if not folders:
+        return [data_dir]
+    return folders
 
-"""# **Setting up Arguments for Test.py**
+def convert_images(style, data_dir, results_root):
+    """ Convert images to line drawings. """
+    conda_env_path = "/n/home02/alvarez/.conda/envs/workshop/bin/python"
+    script_path = 'test.py'
+    repo_root_path = './submodules/informative-drawings'
+    folders = get_folders_or_use_base_dir(data_dir)
+    for folder in progress_bar(folders):
+        dataroot = os.path.join(data_dir, folder)
+        files = sorted(glob(os.path.join(dataroot, "*.JPEG")))
+        results_dir = os.path.join(results_root, folder.replace(data_dir,""))
+        assert results_dir != data_dir, f"Oops, {results_dir} is same as source {data_dir}"
+        os.makedirs(results_dir, exist_ok=True)
+        
+        # make sure these files haven't already been generated
+        existing_files_count = len(glob(os.path.join(results_dir, style, "*.png")))
+        how_many = len(files)
+        if existing_files_count >= how_many:
+            print(f"Skipping {folder} as it already contains the required number of files.")
+            continue
+        
+        print(f"==> Generating {how_many} drawings from {folder}")
+        subprocess.run([conda_env_path, script_path, "--name", style, "--dataroot", dataroot, "--results_dir", results_dir, "--how_many", str(how_many)], cwd=repo_root_path)
+        
+def main():
+    """ Main function to run the script. """
+    args = parse_arguments()    
+    checkpoint_url = 'https://s3.us-east-1.wasabisys.com/visionlab-projects/transfer/informative-drawings/model.zip'
+    download_and_unzip_model(checkpoint_url, './submodules/informative-drawings/checkpoints')
+    convert_images(args.style, args.image_dir, args.output_dir)
 
-This involves iterating through each folder in the dataset, and creating a matching folder of the converted line drawings in the results folder.
-"""
-
-# Check if a results directory exists. If not, create one.
-results_dir_name = "AugmentedDataset"
-results_dir_path = os.path.join(OUTPUT_DIR, results_dir_name)
-os.makedirs(results_dir_path, exist_ok=True)
-
-results_root = os.path.join(OUTPUT_DIR, results_dir_name)
-
-def convert_images(data_dir, results_root, trainval):
-
-    # Create data directory to pull from (either train or val datset)
-    data = os.path.join(data_dir, trainval)
-
-    # Create list of folders in dataset to iterate through.
-    folders = os.listdir(data)
-    print('Folders to iterate through:', folders)
-
-    # Iterate through folders, running test.py on each.
-    for folder in folders:
-        print('\nConverting images in folder:', folder)
-        dataroot = os.path.join(data, folder)
-        files = glob(os.path.join(dataroot, "*.JPEG"))
-
-        results_root = os.path.join(data_dir, 'AugmentedDataset_2', trainval)
-
-        # Create results directory with same name
-        folder_path = os.path.join(results_root, folder)
-        os.makedirs(folder_path)
-
-        # Set results_dir to the folder that matches the subfolder from the dataset
-        results_dir = os.path.join(results_root, folder)
-
-        # Set number of files to convert to line drawings
-        how_many = len(files)-1
-
-        # Run test.py with these parameters
-        script_path = 'informative-drawings/test.py'
-        subprocess.run(["python", script_path, "--name", "anime_style", "--dataroot", dataroot, "--results_dir", results_dir, "--how_many", str(how_many)])
-
-    return 0
-
-# Convert Val dataset
-convert_images(IMAGENET_ROOT_DIR, results_root, 'val')
-
-# Convert train dataset
-convert_images(IMAGENET_ROOT_DIR, results_root, 'train')
-
+if __name__ == "__main__":
+    main()
