6c0afdd6acbf61c910f18a5511007d7232e8cedc
George Alvarez <grez72@gmail.com>
2023-12-08 19:00:47 -0500

    updated history

--- history/08_12_2023.py
+++ history/08_12_2023.py
@@ -77,7 +77,7 @@ val_log = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Li
 test_url = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_rgb/alexnet/20231208_181051/log_test.txt
 
 # ====================================================
-#  RGB model trained with imagenet_rgb_avg stats sgd lr 0.1
+#  RGB model trained with imagenet_rgb_avg stats sgd lr 0.001
 # ====================================================
 
 ./train.sh alexnet in1k_rgb \
@@ -85,19 +85,19 @@ test_url = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-L
     --val_dataset imagenet1k-ffcv/imagenet1k_val_jpg_q100_s256_lmax512_crop.ffcv \
     --test_dataset imagenet1k-line-ffcv/imagenet1k-anime_style_val_jpg_q100_s256_lmax512_crop-872c1585.ffcv \
     --optimizer sgd \
-    --lr 0.1
+    --lr 0.001
 
 checkpoint_path:  /n/holylabs/LABS/alvarez_lab/Users/alvarez/Projects/Project-Line/checkpoint True
 checkpoint_path:  /n/holylabs/LABS/alvarez_lab/Users/alvarez/Projects/Project-Line/checkpoint True
-Submitted job_id: 12189927
-Logs and checkpoints will be saved at local_dir: /n/holylabs/LABS/alvarez_lab/Users/alvarez/Projects/Project-Line/logs/models/in1k_rgb/alexnet/20231208_181606
-Remote Storage Location: s3://visionlab-members/alvarez/Projects/Project-Line/models/in1k_rgb/alexnet/20231208_181606
-train_log = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_rgb/alexnet/20231208_181606/log_train.txt
-val_log = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_rgb/alexnet/20231208_181606/log_val.txt
-test_url = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_rgb/alexnet/20231208_181606/log_test.txt
+Submitted job_id: 12191743
+Logs and checkpoints will be saved at local_dir: /n/holylabs/LABS/alvarez_lab/Users/alvarez/Projects/Project-Line/logs/models/in1k_rgb/alexnet/20231208_183236
+Remote Storage Location: s3://visionlab-members/alvarez/Projects/Project-Line/models/in1k_rgb/alexnet/20231208_183236
+train_log = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_rgb/alexnet/20231208_183236/log_train.txt
+val_log = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_rgb/alexnet/20231208_183236/log_val.txt
+test_url = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_rgb/alexnet/20231208_183236/log_test.txt
 
 # ====================================================
-#  RGB model trained with imagenet_rgb_avg stats sgd lr 0.1
+#  RGB model trained with imagenet_rgb_avg stats sgd lr 0.005
 # ====================================================
 
 ./train.sh alexnet in1k_rgb \
@@ -105,7 +105,16 @@ test_url = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-L
     --val_dataset imagenet1k-ffcv/imagenet1k_val_jpg_q100_s256_lmax512_crop.ffcv \
     --test_dataset imagenet1k-line-ffcv/imagenet1k-anime_style_val_jpg_q100_s256_lmax512_crop-872c1585.ffcv \
     --optimizer sgd \
-    --lr 0.3
+    --lr 0.005
+
+checkpoint_path:  /n/holylabs/LABS/alvarez_lab/Users/alvarez/Projects/Project-Line/checkpoint True
+checkpoint_path:  /n/holylabs/LABS/alvarez_lab/Users/alvarez/Projects/Project-Line/checkpoint True
+Submitted job_id: 12192049
+Logs and checkpoints will be saved at local_dir: /n/holylabs/LABS/alvarez_lab/Users/alvarez/Projects/Project-Line/logs/models/in1k_rgb/alexnet/20231208_183615
+Remote Storage Location: s3://visionlab-members/alvarez/Projects/Project-Line/models/in1k_rgb/alexnet/20231208_183615
+train_log = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_rgb/alexnet/20231208_183615/log_train.txt
+val_log = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_rgb/alexnet/20231208_183615/log_val.txt
+test_url = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_rgb/alexnet/20231208_183615/log_test.txt
 
 # ====================================================
 #  Line-Drawing model trained with imagenet_line_stdonly stats
@@ -126,11 +135,42 @@ val_log = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Li
 test_url = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_anime_style/alexnet/20231208_145131/log_test.txt
 
 # ====================================================
-#  Line-Drawing model trained with imagenet_rgb_avg stats
+#  Line-Drawing model trained with imagenet_line_stdonly stats, sgd lr 0.005
 # ====================================================
 
 ./train.sh alexnet in1k_anime_style \
     --train_dataset imagenet1k-line-ffcv/imagenet1k-anime_style_train_jpg_q100_s256_lmax512_crop-2b7bdbda.ffcv \
     --val_dataset imagenet1k-line-ffcv/imagenet1k-anime_style_val_jpg_q100_s256_lmax512_crop-872c1585.ffcv \
     --test_dataset imagenet1k-ffcv/imagenet1k_val_jpg_q100_s256_lmax512_crop.ffcv \
-    --image_stats_line imagenet_rgb_avg
+    --optimizer sgd \
+    --lr 0.005
+
+checkpoint_path:  /n/holylabs/LABS/alvarez_lab/Users/alvarez/Projects/Project-Line/checkpoint True
+checkpoint_path:  /n/holylabs/LABS/alvarez_lab/Users/alvarez/Projects/Project-Line/checkpoint True
+Submitted job_id: 12193258
+Logs and checkpoints will be saved at local_dir: /n/holylabs/LABS/alvarez_lab/Users/alvarez/Projects/Project-Line/logs/models/in1k_anime_style/alexnet/20231208_184601
+Remote Storage Location: s3://visionlab-members/alvarez/Projects/Project-Line/models/in1k_anime_style/alexnet/20231208_184601
+train_log = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_anime_style/alexnet/20231208_184601/log_train.txt
+val_log = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_anime_style/alexnet/20231208_184601/log_val.txt
+test_url = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_anime_style/alexnet/20231208_184601/log_test.txt
+
+# ====================================================
+#  Line-Drawing model trained with imagenet_rgb_avg stats, sgd lr 0.005
+# ====================================================
+
+./train.sh alexnet in1k_anime_style \
+    --train_dataset imagenet1k-line-ffcv/imagenet1k-anime_style_train_jpg_q100_s256_lmax512_crop-2b7bdbda.ffcv \
+    --val_dataset imagenet1k-line-ffcv/imagenet1k-anime_style_val_jpg_q100_s256_lmax512_crop-872c1585.ffcv \
+    --test_dataset imagenet1k-ffcv/imagenet1k_val_jpg_q100_s256_lmax512_crop.ffcv \
+    --image_stats_line imagenet_rgb_avg \
+    --optimizer sgd \
+    --lr 0.005
+
+checkpoint_path:  /n/holylabs/LABS/alvarez_lab/Users/alvarez/Projects/Project-Line/checkpoint True
+checkpoint_path:  /n/holylabs/LABS/alvarez_lab/Users/alvarez/Projects/Project-Line/checkpoint True
+Submitted job_id: 12193399
+Logs and checkpoints will be saved at local_dir: /n/holylabs/LABS/alvarez_lab/Users/alvarez/Projects/Project-Line/logs/models/in1k_anime_style/alexnet/20231208_184720
+Remote Storage Location: s3://visionlab-members/alvarez/Projects/Project-Line/models/in1k_anime_style/alexnet/20231208_184720
+train_log = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_anime_style/alexnet/20231208_184720/log_train.txt
+val_log = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_anime_style/alexnet/20231208_184720/log_val.txt
+test_url = https://visionlab-members.s3.wasabisys.com/alvarez/Projects/Project-Line/models/in1k_anime_style/alexnet/20231208_184720/log_test.txt
