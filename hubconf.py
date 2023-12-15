344e6a0ecb3891a105af2203b7d752cad2b8f356
grez72 <grez72@gmail.com>
2023-12-15 05:48:53 -0500

    Create hubconf.py
    Add hubconf.py for easy model loading

---
+++ hubconf.py
@@ -0,0 +1,53 @@
+import torch, torchvision
+import importlib.util
+
+# add informative models module
+module_file_path = 'submodules/informative-drawings/model.py'
+module_name = 'informative_drawings'
+spec = importlib.util.spec_from_file_location(module_name, module_file_path)
+models = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(models)
+
+dependencies = ['torch', 'torchvision']
+
+def _load_generator(style, img_size=256):
+  urls = dict(
+      anime_style="https://s3.us-east-1.wasabisys.com/visionlab-members/alvarez/Projects/Project-Line/models/informative-drawings/anime_netG_A_latest-c686ced2.pth",
+      contour_style="https://s3.us-east-1.wasabisys.com/visionlab-members/alvarez/Projects/Project-Line/models/informative-drawings/contour_netG_A_latest-e8b6ec6d.pth",
+      opensketch_style="https://s3.us-east-1.wasabisys.com/visionlab-members/alvarez/Projects/Project-Line/models/informative-drawings/opensketch_netG_A_latest-30a53478.pth"
+  )
+  style_names = list(urls.keys())
+  
+  class ConvertToRGB:
+      def __call__(self, img):
+          return img.convert('RGB')
+      def __repr__(self):
+          return self.__class__.__name__ + '()'
+      
+  assert style in style_names, f"Expected style to be in {style_names}, got {style}"
+  url = urls[style]
+  print(f"==> Loading Generator from: {url}")
+
+  net_G = models.Generator(input_nc=3, output_nc=1, n_residual_blocks=3)
+  
+  state_dict = torch.hub.load_state_dict_from_url(url, map_location='cpu')
+  
+  msg = net_G.load_state_dict(state_dict)
+  print(msg)
+
+  transforms_r = torchvision.transforms.Compose([
+      ConvertToRGB(),
+      torchvision.transforms.Resize(img_size, Image.BICUBIC), 
+      torchvision.transforms.ToTensor()
+  ])
+
+  return net_G, transforms_r
+
+def anime_style(img_size=256):
+  return load_generator("anime_style", img_size=img_size)
+
+def contour_style(img_size=256):
+  return load_generator("contour_style", img_size=img_size)
+
+def opensketch_style(img_size=256):
+  return load_generator("opensketch_style", img_size=img_size)
